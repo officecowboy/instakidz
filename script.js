03c783363a3b781fc40015c2c308fcbcb16af426
Scott Diekema <scott.diekema@gmail.com>
2022-05-09 01:25:30 -0400

    demo app done

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ function menuSelect() {
   const navPics = document.querySelector("#profile")
 
   const post = document.querySelectorAll(".post-container")
+  const upload = document.querySelector(".upload")
 
   navHome.addEventListener("click", function () {
     post[0].style.display = 'initial'
     post[1].style.display = 'initial'
     post[2].style.display = 'initial'
+    upload.style.display = 'none'
   })
 
   navUpload.addEventListener("click", function () {
     post[0].style.display = 'none'
     post[1].style.display = 'none'
     post[2].style.display = 'none'
+    upload.style.display = 'flex'
   })
 
   navPics.addEventListener("click", function () {
     post[0].style.display = 'none'
     post[1].style.display = 'initial'
     post[2].style.display = 'none'
+    upload.style.display = 'none'
   })
 
 }
